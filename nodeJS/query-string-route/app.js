550d07ee8ea22f072256381b85753875672977c4
Ally Deeter <allydeeter6639@gmail.com>
2021-07-19 17:57:18 -0700

    adding query string params

---
+++ nodeJS/query-string-route/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/movies', (req, res) => {
+
+    console.log(req.query.sort);
+    console.log(req.query.page);
+
+    res.send("[Movies]")
+})
+
+app.listen(PORT, () => {
+    console.log("Listening on Port " + PORT);
+})
