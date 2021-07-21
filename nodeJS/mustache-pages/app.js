131452d46bf9bd68870f5b4cd0a44732ea271c62
Ally Deeter <allydeeter6639@gmail.com>
2021-07-20 17:35:52 -0700

    displaying list in mustache pages

--- nodeJS/mustache-pages/app.js
+++ nodeJS/mustache-pages/app.js
@@ -10,6 +10,17 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+app.get('/users', (req, res) => {
+    
+    let users = [
+        {name: "John Doe", age: 34},
+        {name: "Mary Doe", age: 32},
+        {name: "Alex Lowe", age: 27}
+    ]
+
+    res.render('users', {users: users})
+})
+
 app.get('/', (req, res) => {
 
     let user = {
